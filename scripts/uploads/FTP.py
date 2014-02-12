6bdf56dc94cccafc730c75413df7c2a49719b2ef
Jimmie Felidae <evolution.jimmy@gmail.com>
2014-02-12 02:07:28 -0500

    FTP path fix

--- scripts/uploads/FTP.py
+++ scripts/uploads/FTP.py
@@ -2,11 +2,13 @@ def do(addr, user, password, source_directory, target_directory='/'):
     logs = []
     logs.append("[INFO] Uploading to %s ..." % addr)
     import os
+    source_directory = source_directory.rstrip(os.sep)
+    target = target_directory.rstrip('/') + '/' + os.path.basename(source_directory)
     lftp_cmd = """
 open -u %(user)s,%(password)s %(addr)s
 mirror -R %(source)s %(target)s
 """ % {'addr': addr, 'user': user, 'password': password,
-       'source': source_directory, 'target': target_directory}
+       'source': source_directory, 'target': target}
     with open("/tmp/lftp_script", "w") as f:
         f.write(lftp_cmd)
     os.system("lftp -f /tmp/lftp_script")
