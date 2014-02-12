cff12f8dba45d969257a1b994698ecc7bbc68bcb
Jimmie Felidae <evolution.jimmy@gmail.com>
2014-02-12 10:36:11 -0500

    restart + status

--- routine_backup.sh
+++ routine_backup.sh
@@ -4,7 +4,7 @@ start() {
     echo "Starting routine backup: "
     if [ ! -f /tmp/routine_backup.pid ]; then
         touch routine_backup.log
-        echo "================================" > routine_backup.log
+        echo "================================" >> routine_backup.log
         echo $(python -c "from time import strftime;print '[INFO] Starting background routine at %s' % strftime('%Y-%m-%d %H:%M:%S')") >> ./routine_backup.log
         DAYS=`python -c "import config;d=config.ROUTINE_DAYS if 'ROUTINE_DAYS' in config.__dict__ else 7;print d"`
         SECONDS=`echo "${DAYS} * 24 * 60 * 60" | bc`
@@ -28,6 +28,9 @@ stop() {
 status() {
     if [ -f /tmp/routine_backup.pid ]; then
         echo "Running. PID: `cat /tmp/routine_backup.pid`"
+        echo ""
+        echo "Latest logs:"
+        tail -n 10 routine_backup.log
     else
         echo "Stopped."
     fi
@@ -35,14 +38,13 @@ status() {
 
 usage() {
     N=$(basename "$0")
-    echo "Usage: $N {start|stop|status}" >&2
+    echo "Usage: $N {start|stop|status|restart}" >&2
     exit 1
 }
 
 cd $(python -c "import os; print os.path.dirname(os.path.realpath('$0'))")
 
 case "$1" in
-    # If arg `start` is given, start daemon.
     start)
         start
         ;;
@@ -51,6 +53,10 @@ case "$1" in
         ;;
     status)
         status
+        ;;
+    restart)
+        stop
+        start
         ;;
     *)
         usage
