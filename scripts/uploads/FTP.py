0052804868444f8eb6824c71d32faa00f5cba019
Jimmie Felidae <evolution.jimmy@gmail.com>
2014-07-24 11:37:46 -0400

    lftp additional command

--- scripts/uploads/FTP.py
+++ scripts/uploads/FTP.py
@@ -1,14 +1,16 @@
-def do(addr, user, password, source_directory, target_directory='/'):
+def do(addr, user, password, source_directory, target_directory='/', additional_cmd=''):
     logs = []
     logs.append("[INFO] Uploading to %s ..." % addr)
     import os
     source_directory = source_directory.rstrip(os.sep)
     target = target_directory.rstrip('/') + '/' + os.path.basename(source_directory)
     lftp_cmd = """
+%(additional_cmd)s
 open -u %(user)s,%(password)s %(addr)s
 mirror -R %(source)s %(target)s
 """ % {'addr': addr, 'user': user, 'password': password,
-       'source': source_directory, 'target': target}
+       'source': source_directory, 'target': target,
+       'additional_cmd': additional_cmd }
     with open("/tmp/lftp_script", "w") as f:
         f.write(lftp_cmd)
     os.system("lftp -f /tmp/lftp_script")
--- upload-example.py
+++ upload-example.py
@@ -1,4 +1,5 @@
 # Example upload script (optional)
 from scripts.uploads import FTP
 def do(source_directory):
-    return FTP("xxx.xx.xx.xx", "ftp_user", "password", source_directory)
+    return FTP("xxx.xx.xx.xx", "ftp_user", "password", source_directory,
+               additional_cmd="set ssl:verify-certificate no")
